142b17b936e2f6b2eeace5c437cc2fd803c5ec95
Dylan Nguyen <longnguyendinh120505@gmail.com>
2026-09-13 23:49:47 +0700

    feat: reini the project for apple dev

---
+++ SplitPay/App/SplitPayApp.swift
@@ -0,0 +1,19 @@
+//
+//  SplitPayApp.swift
+//  SplitPay
+//
+//  Created by Dinh Long on 13/9/26.
+//
+
+import Foundation
+
+import SwiftUI
+
+@main
+struct SplitPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("SplitPay")
+        }
+    }
+}
