01c931957e9d07addb708d8349caca87d0600d3b
Dylan Nguyen <longnguyendinh120505@gmail.com>
2026-09-16 23:27:36 +0700

    feat: add app DI

---
+++ SplitPay/Core/DI/AppDependencyContainer.swift
@@ -0,0 +1,38 @@
+//
+//  AppDependencyContainer.swift
+//  SplitPay
+//
+//  Created by Dinh Long on 15/9/26.
+//
+
+import Foundation
+import Swinject
+
+final class AppDependencyContainer {
+
+    static let shared = AppDependencyContainer()
+
+    private let container = Container()
+
+    let sessionStore: SessionStore
+
+    private init() {
+        Self.registerServices(in: container)
+
+        sessionStore = container.resolve(SessionStore.self)!
+    }
+
+    private static func registerServices(in container: Container) {
+        container.register(AccountResolving.self) { _ in
+            MockAccountResolvingService()
+        }.inObjectScope(.container)
+
+        container.register(WalletBalanceProviding.self) { _ in
+            MockWalletBalanceProvidingService()
+        }.inObjectScope(.container)
+
+        container.register(SessionStore.self) { resolver in
+            SessionStore(balanceProvider: resolver.resolve(WalletBalanceProviding.self)!)
+        }.inObjectScope(.container)
+    }
+}
---
+++ SplitPay/Module/Transfer/Dependency/TransferDependency.swift
@@ -0,0 +1,13 @@
+//
+//  TransferDependency.swift
+//  SplitPay
+//
+//  Created by Dinh Long on 15/9/26.
+//
+
+import Foundation
+
+struct TransferDependency {
+    let accountResolver: AccountResolving
+    let sessionStore: SessionStore
+}
